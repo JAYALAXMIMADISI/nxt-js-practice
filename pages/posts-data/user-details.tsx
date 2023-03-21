40369a29de8755b4414e7ad1584c8c8063359d27
@jayalaxmi110 <jayalaxmi.madisi@narayanagroup.com>
2023-03-21 15:25:29 +0530

    Users example completed

--- pages/posts-data/user-details.tsx
+++ pages/posts-data/user-details.tsx
@@ -1,37 +1,60 @@
-import React from 'react'
-import { useRecoilValue,selector,atom } from 'recoil';
-import { usersListSelector } from './users-list';
+import React from "react";
+import { useRecoilValue, selector, atom, useRecoilState, RecoilRoot } from "recoil";
+import { usersListSelector } from "./users-list";
+import { useRouter } from "next/router";
+
+console.log("check useRouter", useRouter);
 
 // import {userDetailsSelector} from './user'
 
-const userDetails = atom({
-    key: "makeUser",
-    default: "",
-  });
-  
-export const userDetailsSelector = selector({
-    key: "makeUserSelector",
-    get: ({get}: any) => {
-      const userIdreturn = get(userDetails);
-  
-      const details = useRecoilValue<any>(usersListSelector).filter((item: any) => {
-       return item.id == userIdreturn;
-      });
-      {console.log("check getUserData", details)}
-  
-      return details;
-    },
-  });
+// const userDetails = atom({
+//     key: "makeUser",
+//     default: "",
+//   });
 
+// const det=useRecoilValue(userDetails)
 
-export default function UserDetails(){
-    const getUserData = useRecoilValue(userDetailsSelector);
+export default function UserDetails() {
+  const details: any = useRouter();
+  {
+    console.log("check for id value", details);
+  }
+const {userId,userName}=details.query
+//   const userDetails = atom({
+//     key: "makeUser",
+//     default: "",
+//   });
+//   const userDetailsSelector = selector({
+//     key: "makeUserSelector",
+//     get: async({ get }: any) => {
 
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//         const res = await response.json();
 
-    return(
-        <>
-        {getUserData.title}
-        {getUserData.id}
-        </>
-    )
+//       // const value=useRecoilValue<any>(usersListSelector)
+//     //   const userIdreturn = get(userDetails);
+
+//       const details1 =res.filter((item: any) => {
+//         return item.id == details.query.userId;
+//       });
+//       {
+//         console.log("check getUserData", details);
+//       }
+
+//       return details1;
+//     },
+//   });
+
+  //   const [userId, setUserId] = useRecoilState(userDetails);
+
+//   const idValue = useRecoilValue(userDetailsSelector);
+
+  // const getUserData = useRecoilValue(userDetailsSelector);
+
+  return (
+    <>
+      <h1>{userId}</h1>
+     <h1>{userName}</h1>
+    </>
+  );
 }
--- pages/posts-data/user.tsx
+++ pages/posts-data/user.tsx
@@ -1,37 +1,44 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 import { usersListSelector } from "./users-list";
 import { atom, selector } from "recoil";
-import UserDetails from "./user-details";
+// import UserDetails from "../components/user-details";
 import Link from "next/link";
 
-
-
-
-
-
+export const userClicked = atom({
+  key: "user-click",
+  default: false,
+});
 
 export default function User() {
   const usersList = useRecoilValue<any>(usersListSelector);
 
-
-
-
-  // const [userId, setUserId] = useRecoilState(userDetails);
-
+  const [userClickedStatus, setUserClickedStatus] = useRecoilState(userClicked);
 
   // const getUserDetails = (id: any) => {
   //   setUserId(id);
+  //   setUserClickedStatus(true);
   // };
 
   return (
     <>
+    {/* <RecoilRoot>       */}
       {usersList.map((item: any) => (
         // <div onClick={() => getUserDetails(item.id)}>{item.title}</div>
-        <Link href={'/user-details'}>{item.title}</Link>
+        <Link
+          href={{
+            pathname: "posts-data/user-details",
+            query: { userId: item.id ,userName:item.title} ,
+          }}
+          passHref
+        >
+          <h1>{item.title}</h1>
+        </Link>
       ))}
 
-      {/* <UserDetails /> */}
+      {/* {userClickedStatus && <UserDetails details={idValue[0]}/>} */}
+      {/* </RecoilRoot> */}
+
     </>
   );
 }
